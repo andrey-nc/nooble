bf33f7e38312aa303b49b153a80d5582a469c371
Sergiy Govorukhin <govorukhin@gmail.com>
2016-03-01 21:25:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<h1>nooble - Simple Google clone project</h1>
+
+Functionality:
+ - Full-text indexing of a target page
+ - Search for required word/fraze    
+
+Frameworks 
+ - Spring Boot
+ - Spring MVC
+ - Spring IoC
+ - Lucene
+ - Gradle
+ - FreeMarker
+ 
+Web-server
+ - Apache Tomcat 
---
+++ src/main/java/com/ghost/NoobleApplication.java
@@ -0,0 +1,12 @@
+package com.ghost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoobleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoobleApplication.class, args);
+	}
+}

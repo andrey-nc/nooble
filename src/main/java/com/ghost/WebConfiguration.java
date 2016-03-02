56fb94b14453a1749e56fec696e25f4f40f61261
Sergiy Govorukhin <govorukhin@gmail.com>
2016-03-02 16:35:47 +0200

    Web configuration added

---
+++ src/main/java/com/ghost/WebConfiguration.java
@@ -0,0 +1,10 @@
+package com.ghost;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.ghost")
+public class WebConfiguration {
+
+}

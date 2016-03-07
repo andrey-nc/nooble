50f1805c54a87f3bf83d7756eac0fff3dd0790fc
Sergiy Govorukhin <govorukhin@gmail.com>
2016-03-07 16:14:11 +0200

    Configuration updated

---
+++ src/main/java/com/ghost/FreemarkerConfigurationBean.java
@@ -0,0 +1,4 @@
+package com.ghost;
+
+public class FreemarkerConfigurationBean {
+}
--- src/main/java/com/ghost/NoobleApplication.java
+++ src/main/java/com/ghost/NoobleApplication.java
@@ -5,16 +5,23 @@ import freemarker.template.TemplateExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Locale;
+
 @SpringBootApplication
 public class NoobleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NoobleApplication.class, args);
-		System.out.println("main");
+		freemarkerConfiguration();
+		System.out.println("NoobleApplication - main");
+	}
+
+	private static void freemarkerConfiguration() {
 		Configuration configuration = new Configuration(Configuration.VERSION_2_3_23);
 		configuration.setClassForTemplateLoading(NoobleApplication.class, "templates");
 		configuration.setDefaultEncoding("UTF-8");
-		configuration.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
+		configuration.setLocale(Locale.US);
+		configuration.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
 	}
 
 }

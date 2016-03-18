644e890352e3efbee784b8f9dca640a5ab472d69
Sergiy Govorukhin <govorukhin@gmail.com>
2016-03-18 16:53:53 +0200

    JSON classes implemented

---
+++ src/main/java/com/ghost/json/adapters/UseDataAdapter.java
@@ -0,0 +1,15 @@
+package com.ghost.json.adapters;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Determines class of JsonDataAdapter that should be used to serialize annotated field value.
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UseDataAdapter {
+    Class<? extends JsonDataAdapter> value();
+}

71dcc299c02cbfba584c024c511032a380a9affc
Sergiy Govorukhin <govorukhin@gmail.com>
2016-03-18 16:55:01 +0200

    'View more' feature added using JSON

--- src/main/java/com/ghost/web/SearchController.java
+++ src/main/java/com/ghost/web/SearchController.java
@@ -1,13 +1,11 @@
 package com.ghost.web;
 
 import com.ghost.NoobleApplication;
-import com.ghost.lucene.search.SearchDocument;
+import com.ghost.json.JsonSerializer;
 import com.ghost.lucene.search.SearchService;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.Scope;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -16,13 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
 @Controller
-@Scope("session")
-@PropertySource("classpath:application.properties")
 public class SearchController {
 
     @Autowired
@@ -41,22 +35,40 @@ public class SearchController {
         return "search";
     }
 
+    /**
+     * Indexed docs search using lucene
+     * @param query to search
+     * @param start the number of shown docs on search result page
+     */
     @RequestMapping(value = "/search", method = RequestMethod.GET)
-    public String search(@RequestParam("q") String query, ModelMap model, Locale locale) {
+    public String search(@RequestParam("q") String query,
+                         @RequestParam(name = "start", required = false) Integer start,
+                         ModelMap model,
+                         Locale locale) {
         NoobleApplication.log.info("Search query: {}", query);
+        searchService.getSentQueries().add(query);
         model.addAttribute("appName", environment.getProperty("spring.application.name"));
         model.addAttribute("query", query);
-        List<SearchDocument> list = new ArrayList<>();
-        try {
-            searchService.search(query);
-            list.addAll(searchService.getResultDocs());
-            model.addAttribute("pages", list);
-            model.addAttribute("resultCount", searchService.getTotalHits());
+
+        if (start == null) {
+            try {
+                searchService.search(query);
+                model.addAttribute("pages", searchService.getResultDocs(0));
+                model.addAttribute("resultCount", searchService.getTotalHits());
+                model.addAttribute("searchTime", searchService.getSearchTime());
+                model.addAttribute("start", searchService.getDocsPerPage());
+            } catch (ParseException e) {
+                model.addAttribute("statusError", messageSource.getMessage("search.error.parse", null, locale));
+            } catch (IOException e) {
+                model.addAttribute("statusError", messageSource.getMessage("index.error.directory", null, locale));
+            }
+        }
+        if (start != null) {
+            // to JSON
+            model.addAttribute("newPages", JsonSerializer.serialize(searchService.getResultDocs(start)));
+            model.addAttribute("start", searchService.getDocsPerPage() + start);
             model.addAttribute("searchTime", searchService.getSearchTime());
-        } catch (ParseException e) {
-            model.addAttribute("statusError", messageSource.getMessage("search.error.parse", null, locale));
-        } catch (IOException e) {
-            model.addAttribute("statusError", messageSource.getMessage("index.error.directory", null, locale));
+            model.addAttribute("resultCount", searchService.getTotalHits());
         }
         return "searchResult";
     }
--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -10,3 +10,45 @@ function showStatus(value){
 function clearStatus(){
     document.getElementById("status").innerHTML = "";
 }
+
+function loadDocs() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+            document.getElementById("demo").innerHTML = xhttp.responseText;
+        }
+    };
+    xhttp.open("GET", ".txt", true);
+    xhttp.send();
+}
+
+function ajax() {
+    $(document).on("click", "#viewMore", function () {
+        $.get("search", function (responseText) {
+            $("#newResults").text(responseText);
+        });
+    });
+}
+
+$(document).on("click", "#viewMore", function() {
+    $.get("search", function(responseJson) {
+        var $ul = $("<ul>").appendTo($("#newResults"));
+        $.each(responseJson, function(index, item) {
+            $("<li>").text(item).appendTo($ul);
+        });
+    });
+});
+
+$(document).on("click", "#viewMore", function() {
+    $.get("search", function(responseJson) {
+        var $div = $("<div>").appendTo($("#newResults"));
+        $.each(responseJson, function(index, doc) {
+            $("<h3>").attr("class","page-title").append($("<a>")).attr("href",doc.path).text(doc.title).appendTo($div)
+            $("<div>").attr("class","page-path").text(doc.path).appendTo($div)
+            $("<div>").attr("class","page-preview").text(doc.content).appendTo($div)
+            $("<br>").appendTo($div)
+        });
+    });
+});
+
+
